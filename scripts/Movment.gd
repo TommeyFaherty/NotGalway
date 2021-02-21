971a7b31309dc3323d7d988fc8925383484dfbcc
TommeyFaherty <tommeyf3@hotmail.com>
2021-02-21 12:47:36 +0000

    fixed jump movement
    also added other assets to example scene

--- scripts/Movment.gd
+++ scripts/Movment.gd
@@ -5,14 +5,11 @@ const WALK_SPEED = 100
 
 var velocity = Vector2()
 
-func _physics_process(delta):
+func get_input():
 	
-	velocity.x = 0
-	velocity.y += delta * GRAVITY
-
 	# Animations and movement lumped together 
 	# Best to separate them
-	if Input.is_action_pressed("space"):
+	if is_on_floor() and Input.is_action_pressed("space"):
 		$AnimatedSprite.play("jump")
 	elif Input.is_action_pressed("left_shift") and Input.is_action_pressed(("ui_left")):
 		$AnimatedSprite.play("run_left")
@@ -29,17 +26,23 @@ func _physics_process(delta):
 		velocity.x =  WALK_SPEED
 		$AnimatedSprite.play("walk_right")
 	else:
-		velocity.x = 0
 		$AnimatedSprite.play("default")
 		$AnimatedSprite.flip_h = false
 		
 	# jump if space pressed
 	if Input.is_action_just_pressed("space"):
 		velocity.y = -100
-		
 
+func _physics_process(delta):
+	
+	velocity.x = 0
+	velocity.y += delta * GRAVITY
+	
+	get_input()
 	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
 
 	# The second parameter of "move_and_slide" is the normal pointing up.
 	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
 	move_and_slide(velocity, Vector2(0, -1))
+	velocity = move_and_slide(velocity)
+	print(is_on_floor())
