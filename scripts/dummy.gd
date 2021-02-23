8f912d173bce3a6fdd65f8ffa58c9dcfcb80dff3
peum25 <peum25@gmail.com>
2021-02-23 16:36:52 +0000

    Update Coins
    Coins will collide with one another

--- scripts/dummy.gd
+++ scripts/dummy.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
 
 var ITEM = preload("res://scenes/Item.tscn")
+var no_of_drops = 1
+
+var COIN = preload("res://scenes/Coin.tscn")
+var no_of_coins = 10
 
 
 onready var hp_bar = $TextureProgress
@@ -24,7 +28,7 @@ func UpdateHp(new_hp):
 	#When "new_hp" is negative (taking damage)
 	if new_hp < 0:
 		if current_hp <= 0:
-			Dead()
+			pass
 		else:
 			Hurt()
 	#When hp reaches 0
@@ -36,10 +40,29 @@ func Hurt():
 
 
 func Dead():
+	#Play animation
 	$AnimationPlayer.play("die")
-	var loot_drop = ITEM.instance()
-	get_parent().add_child(loot_drop)
-	loot_drop.position = self.position + Vector2(0,-20)
+	
+	#Drop Items
+	if no_of_drops > 0:
+		var loot_drop = ITEM.instance()
+		get_parent().add_child(loot_drop)
+		loot_drop.position = self.position + Vector2(0,-20)
+		no_of_drops -= 1
+	
+	#Drop Coins
+	var gold_drop = COIN.instance()
+
+	if no_of_coins > 0:
+		get_parent().add_child(gold_drop)
+		gold_drop.position = self.position + Vector2(0,-10)
+		no_of_coins -= 1
+		print(no_of_coins)
+		yield(get_tree().create_timer(0.5), "timeout")
+		return
+		
+	
+	#Remove the dummy from the game
 	yield(get_tree().create_timer(3.0), "timeout")
 	queue_free()
 
@@ -50,6 +73,7 @@ func Hp():
 	#Doesn't let Hp go below 0
 	if current_hp <= 0:
 		current_hp = 0
+		Dead()
 	
 	#Doesn't let Hp go higher than max hp
 	if current_hp > max_hp:
